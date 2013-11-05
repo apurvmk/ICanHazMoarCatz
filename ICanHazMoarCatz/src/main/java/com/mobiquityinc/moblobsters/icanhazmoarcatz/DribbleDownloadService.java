a442aa3fd390c74f30accbf2704d7c0c3fde4e55
Tavon Gatling <tgatling@mobiquityinc.com>
2013-11-05 12:46:31 -0500

    Created initial service

---
+++ ICanHazMoarCatz/src/main/java/com/mobiquityinc/moblobsters/icanhazmoarcatz/DribbleDownloadService.java
@@ -0,0 +1,19 @@
+package com.mobiquityinc.moblobsters.icanhazmoarcatz;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by tgatling on 11/5/13.
+ */
+public class DribbleDownloadService extends IntentService{
+
+    private DribbleDownloadService(){
+        super("hii");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
